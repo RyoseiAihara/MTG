5c97f383d64fa19f3f05ccc2321c59fdac05f597
kari9232 <kari9232@yahoo.co.jp>
2016-05-12 01:22:00 +0900

    update op_turn: set label "Blue" at 49.000000,10.000000

--- test1.js
+++ test1.js
@@ -1,3 +1,24 @@
+//
+//
+//グローバル変数,配列,オブジェクトなど
+//
+//
+var my_hand_cards = [];
+var op_hand_cards = [];
+var life = 20;
+var my_turn = 0;
+var attacker = [];
+var stack = [];
+var void_mana = 0;
+var creaturenum = 0;
+var landnum = 0;
+var landplaycounter = 0;
+var l = cards.length;
+var handnumber = 0;
+var marigancounter = 0;
+var land_manage = [];
+var step = "";
+
 var Card = function() {
   this.name = "";
   this.attr = "";
@@ -6,35 +27,8 @@ var Card = function() {
   this.manacost = null;
   this.src = "";
 }
-var op_cpu = function(){
-  this.life_evalurate = 0;
-  this.permanent_evalurate = 0;
-  this.attack_select = 0;
-  this.deffence_select = 0;
 
-}
-
-var op_turn = function(){
-  step = "op_upkeep";
-  update_display("turntable");
-
-  step = "op_main1";
-  update_display("turntable");
-
-  
-
-}
-
-var update_display = function(display_name){
-  $("#"+display_name+"").empty();
-  if(display_name == "turntable"){
-    $("#"+display_name+"").append("<font size = '3'>step:"+step+"</font><br>");
-  }
-  if(display_name == "manatable"){
-     $("#"+display_name+"").append("<font size = '3'>mana:"+void_mana+"</font><br>");
-  }
-}
-var cards = []; 
+var cards = []; //SQLで実装したい
 for(var i= 0;i < 6;i++){
   cards[i] = new Card();
   cards[i].src = "test_image/mtg"+(i+1)+".jpg";
@@ -48,72 +42,19 @@ for(var i= 0;i < 6;i++){
   }
 }
 
-var life = 20;
-var my_turn = 0;
-var op_turn = 0;
-var attacker = [];
-var stack = [];
-var void_mana = 0;
-var creaturenum = 0;
-var landnum = 0;
-var landplaycounter = 0;
-var l = cards.length;
-var handnumber = 0;
-var marigancounter = 0;
-var land_manage = [];
-var step = "";
+var cpu = function(){
+  this.life_evalurate = 0;
+  this.permanent_evalurate = 0;
+  this.attack_select = 0;
+  this.deffence_select = 0;
 
-var go_to_nextstep = function(steptype){
-  if(step == "stage0"){
-    land_manage_format();
-    $("#life").append("<font size = '3'>life:"+life+"</font><br>");
-  
-  }
-  switch(steptype){
-    case "stage0":step = "upkeep";break;
-    case "upkeep":step = "main1";break;
-    case "main1":step ="selectattacker";break;
-    case "selectattacker":step ="selectdefender";break;
-    case "selectdefender":step ="distribution";break;
-    case "distribution":step = "main2";break;
-    case "main2":step = "endstep";break;
-    case "endstep":op_turn();step = "upkeep";break;
-  }
-  if(step == "upkeep"){
-    next_turn();
-  }
-  void_mana = 0;
-
-  update_display("turntable");
-  update_display("manatable");
-
-};
-var next_turn = function(steptype){
-  step = "upkeep";
-  $("#turntable").empty();
-  $("#turntable").append("<font size = '3'>step:"+step+"</font><br>");
-  void_mana = 0;
-  for(var i=0;i<=creaturenum;i++){
-      untap_it("my_creature",i);
-  }
-  for(var i=0;i<=landnum;i++){
-      untap_it("my_land",i);
-  }
-  land_manage_format();
-  landplaycounter ++;
-  display_mana();
-  draw_card("my");
-  information();
 }
-var start_hand = function(my_or_op){
-  for(i=0;i<7;i++){
-      //append_to_card("hand",cards[i]);
-      draw_card(my_or_op);      
-    } 
-    step = "初手"
-    $("#turntable").append(step);
-    step = "stage0"
-};
+
+//
+//
+//パラーメータ計算関数
+//
+//
 
 var land_manage_format = function(){
   for(var i=0;i<landnum+1;i++){
@@ -129,39 +70,57 @@ var socery_playable = function(steptype){
     return 1;
   }
 }
-var play_land = function(char,cardname){
-  $("#"+char+"_land img").eq(landnum).click(function(){
-    var index2 = $("#"+char+"_land img").index(this);
-    tap_it(""+char+"_land",index2);
-    if(land_manage[index2]==0 ){
-      void_mana ++;
-      add_mana(cards[i]);
-      land_manage[index2] = 1;
-    }else{
-      console.log("already_tapped");
+
+//
+//
+//CPUの行動についての関数
+//
+//
+
+var op_can_cast_land = function(){
+  
+  for(var i=0;i<6;i++){
+    if(op_hand_cards[i].attr == "land"){
+      return i;
     }
-  });
-  handnumber--;
-  landnum ++;
-  landplaycounter --;
-};
-
-var play_creature = function(char,cardname){
-  $("#"+char+"_creature img").eq(creaturenum).click(function(){
-    var index2 = $("#"+char+"_creature img").index(this);
-    if(step == "selectattacker"){
-    tap_it(""+char+"_creature",index2);
-    attacker.push(cards[i]);
-  }else{
-    console.log("no mean select");
   }
-});
-  handnumber--;
-  creaturenum ++;
-  void_mana = void_mana - cards[i].manacost;
-  display_mana();
+  return -1;
 };
 
+var op_cast_land = function(num){
+  append_to_card("op_land",op_hand_cards[num]);
+  $("#op_hand img").eq(num).remove();
+  op_hand_cards.splice(num,1);
+}
+
+
+var op_turn = function(){
+  step = "op_upkeep";
+  update_display("turntable");
+
+  console.log(step);
+  draw_card("op");
+  step = "op_main1";
+  update_display("turntable");
+
+  if(op_can_cast_land() != -1){
+    console.log(op_can_cast_land());
+    op_cast_land(op_can_cast_land());
+  }
+
+  if()
+
+
+  step = "op_endstep";
+  update_display("turntable");
+};
+
+
+//
+//
+//表示についての関数
+//
+//
 var information = function(){
   console.log("mana="+void_mana);
   console.log("hand="+handnumber);
@@ -195,11 +154,133 @@ var append_to_card = function(divname,appended_card){
   $("#"+divname).append("<img name = '"+appended_card.name+"' src = '"+appended_card.src+"' width = '50' height = '80'>");
 };
 
+var update_display = function(display_name){
+  $("#"+display_name+"").empty();
+  if(display_name == "turntable"){
+    $("#"+display_name+"").append("<font size = '3'>step:"+step+"</font><br>");
+  }
+  if(display_name == "manatable"){
+     $("#"+display_name+"").append("<font size = '3'>mana:"+void_mana+"</font><br>");
+  }
+};
+
+//
+//
+//ターン管理の関数
+//
+//
+
+
+var go_to_nextstep = function(steptype){
+  if(step == "stage0"){
+    land_manage_format();
+    $("#life").append("<font size = '3'>life:"+life+"</font><br>");
+  
+  }
+  switch(steptype){
+    case "stage0":step = "upkeep";break;
+    case "op_endstep":step = "upkeep";break;
+    case "upkeep":step = "main1";break;
+    case "main1":step ="selectattacker";break;
+    case "selectattacker":step ="selectdefender";break;
+    case "selectdefender":step ="distribution";break;
+    case "distribution":step = "main2";break;
+    case "main2":step = "endstep";break;
+    case "endstep":op_turn();
+    step = "upkeep";break;
+  }
+  if(step == "upkeep"){
+    next_turn();
+  }
+  void_mana = 0;
+
+  update_display("turntable");
+  update_display("manatable");
+
+};
+var next_turn = function(steptype){
+  step = "upkeep";
+  $("#turntable").empty();
+  $("#turntable").append("<font size = '3'>step:"+step+"</font><br>");
+  void_mana = 0;
+  for(var i=0;i<=creaturenum;i++){
+      untap_it("my_creature",i);
+  }
+  for(var i=0;i<=landnum;i++){
+      untap_it("my_land",i);
+  }
+  land_manage_format();
+  landplaycounter ++;
+  display_mana();
+  draw_card("my");
+  //information();
+}
+
+//
+//
+//ゲーム前のための関数
+//
+//
+var start_hand = function(my_or_op){
+  for(i=0;i<7;i++){
+      //append_to_card("hand",cards[i]);
+      draw_card(my_or_op);      
+    } 
+    step = "初手"
+    $("#turntable").append(step);
+    step = "stage0"
+};
+
+
+//
+//imgとかbutton触ったら動く系
+//.click(function)関数
+//
+//
+
+
+var play_land = function(char,cardname){
+  $("#"+char+"_land img").eq(landnum).click(function(){
+    var index2 = $("#"+char+"_land img").index(this);
+    tap_it(""+char+"_land",index2);
+    if(land_manage[index2]==0 ){
+      void_mana ++;
+      add_mana(cards[i]);
+      land_manage[index2] = 1;
+    }else{
+      console.log("already_tapped");
+    }
+  });
+  handnumber--;
+  landnum ++;
+  landplaycounter --;
+};
+
+var play_creature = function(char,cardname){
+  $("#"+char+"_creature img").eq(creaturenum).click(function(){
+    var index2 = $("#"+char+"_creature img").index(this);
+    if(step == "selectattacker"){
+    tap_it(""+char+"_creature",index2);
+    attacker.push(cards[i]);
+  }else{
+    console.log("no mean select");
+  }
+});
+  handnumber--;
+  creaturenum ++;
+  void_mana = void_mana - cards[i].manacost;
+  display_mana();
+};
+
+
 var draw_card = function(my_or_op){
   var char = my_or_op;
   console.log(char);
   var l = cards.length;
   var r = Math.floor(Math.random()*l);
+  if(my_or_op == "my"){my_hand_cards.push(cards[r]);}
+  if(my_or_op == "op"){op_hand_cards.push(cards[r]);}
+  
   append_to_card(""+char+"_hand",cards[r]);
   $("#"+char+"_hand img").eq(handnumber).click(function(e){
       var index = $("#"+char+"_hand img").index(this);
@@ -211,6 +292,7 @@ var draw_card = function(my_or_op){
             if(cards[i].attr == "land"){  
               if(landplaycounter >= 1){
                 append_to_card(char+"_land",cards[i]);
+                my_hand_cards.splice(index,1);
                 $("#"+char+"_hand img").eq(index).remove();
                 play_land("my",cards[i]);       
                 
@@ -220,6 +302,7 @@ var draw_card = function(my_or_op){
             if(cards[i].attr == "creature"){
               if(void_mana >= cards[i].manacost){                
                 append_to_card(""+char+"_creature",cards[i]);
+                my_hand_cards.splice(index,1);
                 $("#"+char+"_hand img").eq(index).remove();
                 play_creature("my",cards[i]);
                 break;
@@ -281,7 +364,7 @@ $('#nextstep').click(function(){
   if(step == "selectattacker"){
     //$("#turntable").append("<input id = 'select' type = 'button' value = 'select_attack_creature '>");
   }
-  information();
+ // information();
 });
 $('#start').click(function(){
   $("#my_hand img").remove();
